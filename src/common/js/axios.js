9ed11fcb3dfeecb12e2afb6582f674136a4d68ec
wuchengcheng <you@example.com>
2019-01-04 00:04:01 +0800

    接口数据

---
+++ src/common/js/axios.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import qs from 'qs';
+
+export default function request(url, type, params) {
+    if (type === 'post') {
+        params=qs.stringify(params)
+    }
+    return axios({ url, method: type, data: params });
+}
+
+// 添加请求拦截器
+/*
+axios.interceptors.request.use(
+    (config) => {
+        return config;
+    },
+    (error) =>{
+        Promise.reject(error)
+    }
+);
+*/
+
+// 添加响应拦截器
+axios.interceptors.response.use(
+    response =>{
+        return response.data;
+    },
+    error =>{
+        return Promise.reject(error);
+    }
+);
--- src/pages/article/addCategory.vue
+++ src/pages/article/addCategory.vue
@@ -31,7 +31,9 @@
         </div>
     </div>
 </template>
+
 <script>
+    import {getCategoryList} from "../../common/js/http";
     export default{
         name:'add-article',
         data(){
@@ -55,10 +57,17 @@
                 ]
             }
         },
+        created(){
+            getCategoryList().then(res=>{
+                console.log(res);
+                let tmp={"code":0,"data":[{"_id":"5c2cc567996f3446cfd5caf4","title":"用koa2当后台","description":"使用koa2当后台，给前端提供接口","keywords":"koa2,后台","pid":"0","add_time":123456789},{"_id":"5c2e0925a80cdf129274cd7d","title":"koa-router","description":"使用koa-router来路由，方便页面跳转","keywords":"koa2,路由,router","pid":"5c2cc567996f3446cfd5caf4","add_time":122456789},{"_id":"5c2e09a3a80cdf129274cd7e","title":"koa-static","description":"使用koa-static来静态资源托管，方便访问","keywords":"koa2,静态资源,static","pid":"5c2cc567996f3446cfd5caf4","add_time":122456189},{"_id":"5c2e0a28a80cdf129274cd7f","title":"vue","description":"使用vue2做框架，快速开发","keywords":"vue2,前端","pid":"0","add_time":12244619},{"_id":"5c2e0ab0a80cdf129274cd80","title":"axios","description":"使用axios做ajax，快速访问接口","keywords":"axios,前端,vue2","pid":"5c2e0a28a80cdf129274cd7f","add_time":12244619},{"_id":"5c2e0b0fa80cdf129274cd81","title":"eslint","description":"使用eslint规范前端代码,代码风格统一","keywords":"eslint,前端","pid":"5c2e0a28a80cdf129274cd7f","add_time":12244619},{"_id":"5c2e0b56a80cdf129274cd82","title":"webpack","description":"webpack来打包项目，减少二次请求","keywords":"webpack,前端","pid":"5c2e0a28a80cdf129274cd7f","add_time":12244619},{"_id":"5c2e0beca80cdf129274cd83","title":"测试","description":"使用禅道来提交的项目代码bug","keywords":"测试,前端,禅道","pid":"0","add_time":12244619}]};
+            })
+        },
         methods:{
 
         }
     }
+    /* ObjectId("5c2e0a28a80cdf129274cd7f") */
 </script>
 <style lang="scss" type="text/scss" scoped>
     @import '../../common/scss/variable';
@@ -86,6 +95,8 @@
             }
             .long{
                 width:  300px;
+                font-size: $font-size-medium;
+                padding-left: .5em;
                 &.text{
                     height: 100px;
                     resize: none;
@@ -93,7 +104,7 @@
             }
         }
         .extra-tbn{
-            width: 386px;
+            width: 398px;
             background-color: $color-sub-theme;
             height: 40px;
             line-height: 40px;
